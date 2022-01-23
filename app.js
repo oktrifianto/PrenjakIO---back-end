e68abc6231e40ff510f6515a4bf526ac82b4310b
Rahmat <oktrifianto@gmail.com>
2022-01-24 04:55:15 +0700

    Init App
    Install express.js
    Install nodemon for development
    Create .gitignore file
    Create .env - environment variable

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app     = express()
+const port    = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    "data" : "Hello Prenjak IO"
+  })
+})
+
+app.post('/products', (req, res) => {
+  res.json({
+    "data" : "Success for POST method"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`)
+})
