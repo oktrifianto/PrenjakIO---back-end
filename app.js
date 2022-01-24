11badf9febe593c58953afaae80ad8fab2618d30
Rahmat <oktrifianto@gmail.com>
2022-01-25 03:03:00 +0700

    Database: Testing MySQL Connection

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const app     = express()
 const port    = 3000
 require('dotenv').config()
 
+const con = mysql.createConnection({
+  host      : process.env.DB_HOST,
+  user      : process.env.DB_USER,
+  password  : process.env.DB_PASSWORD,
+  database  : process.env.DB_NAME
+})
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log("MySQL Connected")
+})
+
 app.get('/', (req, res) => {
   res.json({
     "data" : "Hello Prenjak IO"
