bbb2877dff0b2d0b0f9fdc288ea2639f243728bf
Rahmat <oktrifianto@gmail.com>
2022-01-25 03:37:38 +0700

    Update: add semicolon for more readable

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const db      = require('./db/database')
-const app     = express()
-const port    = 3000
+const express = require('express');
+const db      = require('./db/database');
+const app     = express();
+const port    = 3000;
 
 app.get('/', (req, res) => {
   res.json({
     "data" : "Hello Prenjak IO"
-  })
-})
+  });
+});
 
 app.post('/products', (req, res) => {
   res.json({
     "data" : "Success for POST method"
-  })
-})
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Application listening on port ${port}`)
-})
+  console.log(`Application listening on port ${port}`);
+});
