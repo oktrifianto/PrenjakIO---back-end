3b033f82255bfd663c5d004cdb6182334adb0711
Rahmat <oktrifianto@gmail.com>
2022-01-25 03:23:30 +0700

    Remove DB Config to another file

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 const express = require('express')
-const mysql   = require('mysql')
+const db      = require('./db/database')
 const app     = express()
 const port    = 3000
-require('dotenv').config()
-
-const con = mysql.createConnection({
-  host      : process.env.DB_HOST,
-  user      : process.env.DB_USER,
-  password  : process.env.DB_PASSWORD,
-  database  : process.env.DB_NAME
-})
-
-con.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL Connected")
-})
 
 app.get('/', (req, res) => {
   res.json({
