f0fc4f0992f50e6e4a85501a6e46c207032837b3
Rahmat <oktrifianto@gmail.com>
2022-02-04 13:16:51 +0700

    Feature: get single product
    Check ID product exist
    Return single product
    Handling if product not found

--- routes/product.js
+++ routes/product.js
@@ -51,4 +51,64 @@ router.post('/new', auth, (req, res) => {
   }
 });
 
+/**
+ * @description   Show single data
+ * @method        GET
+ * @path          /product/:id
+ * @example       /product/2
+ * @public
+ */
+router.get('/:id', async (req, res) => {
+  try {
+    const id      = req.params.id;
+    const checkId = await checkIDProductExist(id);
+    if (checkId){
+      const data  = await getSingleProduct(id);
+      res.status(200).json({
+        "status"  : res.statusCode,
+        "data"    : data
+      });
+    } else {
+      res.status(404).json({
+        "status"  : res.statusCode,
+        "message" : "product not found"
+      });
+    }
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+/**
+ * @description   Get single product
+ * @param         {id} 
+ * @returns       {id, name, price, image_link, rating, desc, qty}
+ */
+const getSingleProduct = id => {
+  const sql = `SELECT * FROM product WHERE id="${id}"`;
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, result) => {
+      if (err) throw reject(err);
+      resolve(result[0]);
+    });
+  });
+}
+
+/**
+ * @description     Check ID Product if exist
+ * @returns         boolean (true/false)
+ *                  (true)    ID exist
+ *                  (false)   ID not exist
+ * @param           {id} from product
+ */
+const checkIDProductExist = id => {
+  const sql = `SELECT id FROM product WHERE id="${id}"`;
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, result) => {
+      if (err) throw reject(err);
+      result.length !== 0 ? resolve(true) : resolve(false);
+    });
+  });
+}
+
 module.exports = router;
