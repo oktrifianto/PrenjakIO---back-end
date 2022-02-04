f934b537c8ff03a4e316af9946990e99ce2fc566
Rahmat <oktrifianto@gmail.com>
2022-02-04 07:12:09 +0700

    Product: create new product
    Success save to db
    Success to create new one product
    Success using auth

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 const db      = require('../db/database');
+const auth    = require('../middleware/auth');
+router.use(express.json());
 
 /**
  * @desc Show all products
@@ -18,6 +20,32 @@ router.get('/', (req, res) => {
         "data"    : result
       });
     });
+  } catch(err){
+    console.log(err);
+  }
+});
+
+/**
+ * @description   Create new product
+ * @method        POST
+ * @path          /product/new
+ * @protected
+ */
+router.post('/new', auth, (req, res) => {
+  try {
+    const { name, price, image, rating, description, qty } = req.body;
+    const sql = `INSERT INTO 
+      product (name, price, image, rating, description, qty) 
+      VALUES ('${name}', ${price}, '${image}', ${rating}, '${description}', ${qty})`;
+    
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(201).json({
+        "status"  : res.statusCode,
+        "message" : "success create new product"
+      });
+    });
+    
   } catch(err){
     console.log(err);
   }
