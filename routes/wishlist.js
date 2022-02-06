9f9189877c77290f2290168893033ed7c2016785
Rahmat <oktrifianto@gmail.com>
2022-02-06 18:17:51 +0700

    Feature: success added product to user wishlist

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -16,4 +16,34 @@ router.get('/', (req, res) => {
   });
 });
 
+/**
+ * @todo
+ * 1. add wislist product (auth) (√)
+ * 2. show wishlist from user  
+ * 3. remove from wishlist (auth)
+ * 4. add to cart --- quantities feature
+ */
+
+/**
+ * @description     Add wishlist product
+ * @method          POST
+ * @path            /wishlist/:id_product/create/:id_user
+ * @todo            show product name
+ */
+router.post('/:id_product/add/:id_user', (req, res) => {
+  try {
+    const { id_product, id_user } = req.params;
+    const sql = `INSERT INTO wishlist (id_from_product, id_from_user) VALUES ('${id_product}', '${id_user}')`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(201).json({
+        "status"    : res.statusCode,
+        "message"   : "success add your product to wishlist"
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
