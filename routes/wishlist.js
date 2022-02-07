41de36b0401b0da7b023f6c5accbb22ecc6ba798
Rahmat <oktrifianto@gmail.com>
2022-02-07 14:24:09 +0700

    Update: refactor wishlist function to controllers

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const db      = require('../db/database');
 const auth    = require('../middleware/auth');
+const lib     = require('../controllers/wishlist.controllers');
 router.use(express.json());
 
 /**
@@ -28,7 +29,8 @@ router.get('/', (req, res) => {
  * @description     Add wishlist product
  * @method          POST
  * @path            /wishlist/:id_product/create/:id_user
- * @todo            show product name
+ * @todo            show product name     ex. product x is successfully added to wishlist
+ *                  handle duplicate wishlist
  */
 router.post('/:id_product/add/:id_user', (req, res) => {
   try {
@@ -53,19 +55,19 @@ router.post('/:id_product/add/:id_user', (req, res) => {
  * @requires        getIDUser(),getArrayWishlist(),getUserWishlist()
  *                 
  */
- router.get('/:username', async (req, res) => {
+router.get('/:username', async (req, res) => {
   try {
     const username  = req.params.username;
-    const id_user   = await getIDUser(username);
+    const id_user   = await lib.getIDUser(username);
 
-    if (await checkWishlistExist(id_user)){
+    if (await lib.checkWishlistExist(id_user)){
       // get array of wishlist
-      const arrayWishlist = await getArrayWishlist(id_user); // array => id_wishlist
+      const arrayWishlist = await lib.getArrayWishlist(id_user); // array => id_wishlist
       
       // looping to get wishlist one by one
       let data = [];
       for (let i = 0; i < arrayWishlist.length; i++){
-        data.push(await getUserWishlist(arrayWishlist[i]));
+        data.push(await lib.getUserWishlist(arrayWishlist[i]));
       }
 
       res.status(200).json({
@@ -82,77 +84,5 @@ router.post('/:id_product/add/:id_user', (req, res) => {
     console.log(err);
   }
 });
-
-
-///////////////////////////////////
-///////// FUNCTIONS ///////////////
-///////////////////////////////////
-/**
- * @description   Get ID from user using username
- * @param         {username}
- * @returns       {id}
- */
-const getIDUser = username => {
-  const sql   = `SELECT id FROM user WHERE username='${username}'`;
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) throw reject(err);
-      resolve(result[0].id);
-    });
-  });
-}
-
-/**
- * @description   Get Data Wishlist in Array
- * @param         {id} 
- * @returns       {Array}
- *                [ '1', '2', '4' ]
- */
-const getArrayWishlist = id => {
-  const sql = `SELECT id_from_product FROM wishlist WHERE id_from_user='${id}'`;
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) throw reject(err);
-      if (result.length > 0) {
-        let arrayWislist = [];
-        for(let i = 0; i < result.length; i++){
-          arrayWislist.push(result[i].id_from_product); // push result to array
-        }
-        resolve(arrayWislist);
-      }
-    });
-  });
-}
-
-/**
- * @description   Show one of user wishlist
- * @param         {id} 
- * @returns       single array
- *                {id, name, price, rating, etc.}       
- */
-const getUserWishlist = id => {
-  const sql = `SELECT * FROM product WHERE id="${id}"`;
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) throw reject(err);
-      resolve(result[0]);
-    });
-  });
-}
-
-/**
- * @description Check Wishlist Existed or Not
- * @param       {id}
- * @returns     boolean
- */
-const checkWishlistExist = id => {
-  const sql = `SELECT * FROM wishlist WHERE id_from_user="${id}"`;
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) throw reject(err);
-      result.length > 0 ? resolve(true) : resolve(false);
-    });
-  });
-}
 
 module.exports = router;
